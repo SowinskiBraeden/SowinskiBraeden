a2c729869c8842312489c37e833502b3fbdcef5d
Braeden Sowinski <48144618+Braeden57@users.noreply.github.com>
2021-07-06 20:58:40 -0700

    Change PORT

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-const PORT = 8080
+const PORT = 3000
 const server = app.listen(PORT, function () {
 	console.log('Server started.', server.address());
 });
