72418875e1a3255f37853f4e448663c26d05d253
Braeden57 <sowinskibraeden@gmail.com>
2021-07-06 20:22:58 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+const PORT = 8080
+const server = app.listen(PORT, function () {
+	console.log('Server started.', server.address());
+});
+
+require('./app/routes')(app);
