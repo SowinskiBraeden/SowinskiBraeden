85d2e745064bcd739cdec68995e9ec1bc439899b
Braeden57 <sowinskibraeden@gmail.com>
2021-07-07 17:06:00 -0700

    Added Custom 404 Page

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,8 @@ module.exports = function (app) {
   app.get('/about', function (req, res) {
     res.render('about');
   });
+  //The 404 Route (ALWAYS Keep this as the last route)
+  app.get('*', function(req, res){
+    res.status(404).render('page-not-found');
+  });
 }
