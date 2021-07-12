0b4d7b88724f7db58545f12c76fd4949b68a6a85
Braeden57 <sowinskibraeden@gmail.com>
2021-07-12 12:45:31 -0700

    User HTML View Engine

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
+const cons = require('consolidate');
 const app = express();
 
-app.set('view engine', 'ejs');
+// view engine setup
+app.engine('html', cons.swig)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 const server = app.listen(PORT, function () {
 	console.log('Server started.', server.address());
 });
