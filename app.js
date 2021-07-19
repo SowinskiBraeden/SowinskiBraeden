3c19c65d1c6b96e67e844d79d7f441d3006ee85b
Braeden57 <sowinskibraeden@gmail.com>
2021-07-18 22:55:51 -0700

    Remove www?

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
+let wwwRedirect = function(req, res) {
+	if(req.get('host').indexOf('www.') === 0){
+		if(req.method === "GET" && !req.xhr){
+			return res.redirect(req.protocol + '://' + req.get('host').substring(4) + req.originalUrl);
+		}
+	}
+}
+
+app.use(wwwRedirect);
+
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 8000;
