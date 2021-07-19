5afd9279739bf6163c99bf546c94b4390e759383
Braeden57 <sowinskibraeden@gmail.com>
2021-07-18 23:27:59 -0700

    some update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cons = require('consolidate');
+const redirectSSL = require('redirect-ssl');
 const app = express();
 
 // view engine setup
@@ -8,6 +9,9 @@ app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
+app.use(redirectSSL.create({
+	exclude: ['localhost']
+}));
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 8000;
