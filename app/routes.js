f3783a0f06d829bd4911c9417643c57aed84f2f0
Braeden57 <sowinskibraeden@gmail.com>
2021-09-01 14:37:39 -0700

    Update Routes

--- app/routes.js
+++ app/routes.js
@@ -1,15 +1,27 @@
 module.exports = function (app) {
-  app.get('/', function (req, res) {
+  app.get('/', (req, res) => {
     res.render('index');
   });
-  app.get('/about', function (req, res) {
+
+  app.get('/about', (req, res) => {
     res.render('about');
   });
-  app.get('/projects', function (req, res) {
+
+  app.get('/projects', (req, res) => {
     res.render('projects');
   });
+
   //The 404 Route (ALWAYS Keep this as the last route)
-  app.get('*', function(req, res){
+  app.get('*', (req, res) => {
     res.status(404).render('page-not-found');
+  });
+
+  /*
+    My Domain provider redirects to defaultsite
+    when accessing the http site.
+    This will redirect back to home page.  
+  */
+  app.get('/defaultsite', (req, res) => {
+    res.redirect('/');
   });
 }
